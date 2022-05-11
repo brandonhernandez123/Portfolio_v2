82cd9750551f8cd884115cdd38a20ab9d49ec3df
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-11 15:45:18 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Topnav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Topnav />
+      </nav>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div id="section1">
+        <h2>Section 1</h2>
+      </div>
+      <div id="section2">
+        {' '}
+        <h2>Section 2</h2>
+      </div>
+      <div id="section3">
+        {' '}
+        <h2>Section 3</h2>
+      </div>
+      <div id="section4">
+        {' '}
+        <h2>Section 4</h2>{' '}
+      </div>
+      <div id="section5">
+        {' '}
+        <h2>Section 5</h2>
+      </div>
+      <div id="contact">
+        <h2>Contact</h2>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { Nav, Container, Navbar, NavDropdown } from "react-bootstrap";
+import { Link } from "react-scroll/modules";
+import logo from '../images/logo.png'
+
+
+const Topnav = () => {
+    return(
+        <Navbar collapseOnSelect expand="lg" className="nav">
+        <Container>
+        <Navbar.Brand href="#home">
+        <img
+        src={logo}
+        width="250"
+        height="250"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+
+
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+           
+           
+          </Nav>
+          <Nav>
+              <Link
+                    activeClass="active"
+                    to="section1"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={0}>
+            <Nav.Link className='navlink' href="#about">
+                
+                                
+                     
+          
+                 
+                About
+                
+            </Nav.Link>  
+            </Link>   
+            <Link
+          activeClass="active"
+          to="section2"
+         spy={true}
+         smooth={true}
+         offset={-100}
+         duration={0}>
+            <Nav.Link className='navlink' eventKey={2} href="#experience">
+           
+          
+          
+          
+           
+             Experience
+            </Nav.Link>
+            </Link>
+            <Link
+          activeClass="active"
+          to="section3"
+         spy={true}
+         smooth={true}
+         offset={-100}
+         duration={0}>
+          
+          
+          
+          
+            <Nav.Link className='navlink' href="#projects">Projects</Nav.Link>
+            </Link> 
+            <Link
+          activeClass="active"
+          to="section4"
+         spy={true}
+         smooth={true}
+         offset={-100}
+         duration={0}>
+          
+          
+          
+          
+            <Nav.Link  className='navlink' eventKey={2} href="#tech">
+              Tech
+            </Nav.Link>
+            </Link> 
+            
+            <Nav.Link  className='navlink' eventKey={2} href="/resume">
+              Resume
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+}
+
+export default Topnav;
