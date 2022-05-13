55f9552c4b708c7fab5fdd279c8f641234897124
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-12 20:39:25 -0600

    added loading screen

--- src/App.js
+++ src/App.js
@@ -2,51 +2,65 @@ import './App.css'
 import About from './components/About'
 import Landing from './components/Landing'
 import Topnav from './components/Nav'
+import Loading from './components/Loading'
+import { useState, useEffect } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <nav>
-        <Topnav />
-      </nav>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+  const [loading, setLoading] = useState(true)
 
-      <div className="landing">
-        <Landing />
-      </div>
-      <br />
-      <div id="section1">
-        <About />
-      </div>
-      <div id="section2">
-        {' '}
-        <h2>Section 2</h2>
-      </div>
-      <div id="section3">
-        {' '}
-        <h2>Section 3</h2>
-      </div>
-      <div id="section4">
-        {' '}
-        <h2>Section 4</h2>{' '}
-      </div>
-      <div id="section5">
-        {' '}
-        <h2>Section 5</h2>
-      </div>
-      <div id="contact">
-        <h2>Contact</h2>
-      </div>
-    </div>
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 6000)
+  }, [])
+
+  return (
+    <>
+      {loading === false ? (
+        <div className="App">
+          <nav>
+            <Topnav />
+          </nav>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+
+          <div className="landing">
+            <Landing />
+          </div>
+          <br />
+          <div id="section1">
+            <About />
+          </div>
+          <div id="section2">
+            {' '}
+            <h2>Section 2</h2>
+          </div>
+          <div id="section3">
+            {' '}
+            <h2>Section 3</h2>
+          </div>
+          <div id="section4">
+            {' '}
+            <h2>Section 4</h2>{' '}
+          </div>
+          <div id="section5">
+            {' '}
+            <h2>Section 5</h2>
+          </div>
+          <div id="contact">
+            <h2>Contact</h2>
+          </div>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   )
 }
 
---
+++ src/components/Loading.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import loading from '../images/loading.gif'
+
+
+const Loading = () => {
+    return(
+        <div className='loading'>
+            <img src={loading} />
+            
+        </div>
+    )
+}
+
+export default Loading;
