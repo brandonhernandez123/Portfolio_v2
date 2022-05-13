f233ba29df7d6085abdd27995f7a87ab0a27b594
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-13 14:29:00 -0600

    working on experience section and added new dependencies

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
 
 
 const Experience = () => {
@@ -7,8 +10,17 @@ const Experience = () => {
         <Container className='section2' fluid>
             <h2>Experience</h2>
          <Row>
-            <Col>
+            <Col className='sei'>
             
+            <h2><FontAwesomeIcon icon="fa-solid fa-terminal" /> Software Engineering Experience</h2>
+            <br/>
+            <h3>Software Engineering Apprentice</h3>
+            <h4>General Assembly</h4>
+            <h6>July 2021 - Oct 2021</h6>
+            <ul>
+                <li>Applied practiced full-stack software engineering concepts to develop intuitive, responsive, and secure web applications over a 500+ hours/12 weeks software development immersive.</li>
+                <li>Designed and built full-stack applications while leveraging architectural modeling, prototyping, and version control tools for individual and collaborative projects.</li>
+            </ul>
             </Col>
             <Col>
             
