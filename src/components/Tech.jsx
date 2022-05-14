0ca8b4183b9749e591b583b276d307b90d444a05
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-14 16:46:03 -0600

    completed tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -30,14 +30,25 @@ const Tech = () => {
                 <Image src='https://i.imgur.com/SNvxaLc.png' height={64} width={64} alt='bootstrap' className='languages' />
                 </Col>
             </Row>
+            <br/>
             <Row>
-                <h5>Backend</h5>
+                <h5>Backend & Databases</h5>
                 <Col>
                 <Image  src='https://i.imgur.com/iHdseRT.png' height={64} width={64} alt='Node' className='languages'  />
                 <Image src='https://i.imgur.com/gul247b.png' height={64} width={64} alt='ExpressJs' className='languages' />
                 <Image src='https://i.imgur.com/Ee9JBP0.png' height={64} width={64} alt='MongoDb' className='languages' />
                 <Image src='https://i.imgur.com/xcoRtim.png' height={64} width={64} alt='Flask' className='languages' />
                 <Image src='https://i.imgur.com/MEprI7G.png' height={64} width={64} alt='Postgresql' className='languages' />
+                </Col>
+            </Row>
+            <br/>
+            <Row>
+                <h5>Other</h5>
+                <Col>
+                <Image  src='https://i.imgur.com/HvVGmPd.png' height={74} width={64} alt='Heroku' className='languages'  />
+                <Image src='https://i.imgur.com/ilgJ9pW.jpg' height={64} width={64} alt='Vercel' className='languages' />
+                <Image src='https://i.imgur.com/HLYSEq4.jpg' height={64} width={64} alt='Restful API' className='languages' />
+                <Image src='https://i.imgur.com/yUyjL9h.png' height={64} width={70} alt='Github' className='languages' />
                 </Col>
             </Row>
 
